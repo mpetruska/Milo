4c6faa4a81f9e0050a45bb9f564096d6d43bcf64
Alexander Ivanov <4lex1v@gmail.com>
2015-08-18 19:18:44 +0100

    Simplify protocol for the time being

--- src/main/scala/milo/device/DeviceData.scala
+++ src/main/scala/milo/device/DeviceData.scala
@@ -2,32 +2,11 @@ package milo.device
 
 import scodec._, bits._, codecs._
 
-final case class DeviceDataFrame(
-  `type`: Byte,
-  unit: Byte,
-  data: ByteVector)
-
-object DeviceDataFrame {
-  implicit val frameCodec: Codec[DeviceDataFrame] = (
-    ("t"     | byte) ::
-    ("u"     | byte) ::
-    ("value" | bytes(8))
-  ).as[DeviceDataFrame]
-
-  val ddfList: Codec[List[DeviceDataFrame]] = list(frameCodec)
-}
-
-final case class DeviceData(
-  time: Long,
-  seq: Int,
-  length: Short,
-  data: List[DeviceDataFrame])
+final case class DeviceData(seq: Int, data: ByteVector)
 
 object DeviceData {
-  implicit val dataCodec: Codec[DeviceData] = (
-    ("time" | int64) ::
+  implicit val dataCodec = (
     ("seq"  | int32) ::
-    ("cnt"  | short16) ::
-    ("data" | DeviceDataFrame.ddfList)
+    ("data" | bytes(8))
   ).as[DeviceData]
 }
