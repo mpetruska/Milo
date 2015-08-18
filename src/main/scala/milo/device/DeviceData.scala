ff00d88d046916a081f37de52481e302e2f480de
Alexander Ivanov <4lex1v@gmail.com>
2015-08-18 16:08:55 +0100

    Now it compiles !!

--- src/main/scala/milo/device/DeviceData.scala
+++ src/main/scala/milo/device/DeviceData.scala
@@ -1,14 +1,34 @@
 package milo.device
 
-import scodec.bits.ByteVector
+import scodec._, bits._, codecs._
 
 final case class DeviceDataFrame(
   `type`: Byte,
   unit: Byte,
   data: ByteVector)
 
+object DeviceDataFrame {
+  implicit val frameCodec: Codec[DeviceDataFrame] = (
+    ("t"     | byte) ::
+    ("u"     | byte) ::
+    ("value" | bytes(8))
+  ).as[DeviceDataFrame]
+
+  val ddfList: Codec[List[DeviceDataFrame]] = list(frameCodec)
+
+}
+
 final case class DeviceData(
   time: Long,
   seq: Int,
   length: Short,
   data: List[DeviceDataFrame])
+
+object DeviceData {
+  implicit val dataCodec: Codec[DeviceData] = (
+    ("time" | int64) ::
+    ("seq"  | int32) ::
+    ("cnt"  | short16) ::
+    ("data" | DeviceDataFrame.ddfList)
+  ).as[DeviceData]
+}
--- src/main/scala/milo/device/DeviceId.scala
+++ src/main/scala/milo/device/DeviceId.scala
@@ -2,7 +2,10 @@ package milo.device
 
 import java.util.UUID
 
-import scodec.bits.ByteVector
-import scodec.{Decoder, Codec}
+import scodec._, bits._, codecs._
 
 final case class DeviceId(id: UUID) extends AnyVal
+
+object DeviceId {
+  implicit val deviceIdCodec = uuid.map(DeviceId(_))
+}
